639a37d4303ad8486f37553d74e8e747e3389432
Chihiro WATANABE <chihiro.watanabe@live.jp>
2026-07-08 15:42:45 +0900

    refactor: remove hostname and tailscale_ip from sys module and tests

--- src/sys.rs
+++ src/sys.rs
@@ -1,43 +1,5 @@
 use std::process::Command;
 
-pub fn hostname() -> String {
-    Command::new("hostname")
-        .output()
-        .ok()
-        .and_then(|o| String::from_utf8(o.stdout).ok())
-        .map(|s| s.trim().to_string())
-        .unwrap_or_default()
-}
-
-pub fn tailscale_ip() -> String {
-    #[cfg(target_os = "linux")]
-    {
-        Command::new("ip")
-            .args(["-4", "addr", "show", "dev", "tailscale0"])
-            .output()
-            .ok()
-            .and_then(|o| String::from_utf8(o.stdout).ok())
-            .and_then(|output| {
-                output.lines().find_map(|line| {
-                    let trimmed = line.trim();
-                    if trimmed.starts_with("inet ") {
-                        trimmed
-                            .split_whitespace()
-                            .nth(1)
-                            .map(|addr| addr.split('/').next().unwrap_or("").to_string())
-                    } else {
-                        None
-                    }
-                })
-            })
-            .unwrap_or_default()
-    }
-    #[cfg(not(target_os = "linux"))]
-    {
-        String::new()
-    }
-}
-
 pub fn power_status() -> (bool, Option<u8>) {
     #[cfg(target_os = "linux")]
     {
--- tests/sys_tests.rs
+++ tests/sys_tests.rs
@@ -1,19 +1,6 @@
 use statusline::sys;
 
 #[test]
-fn hostname_returns_string() {
-    let name = sys::hostname();
-    // Should return something (even empty is fine - just shouldn't panic)
-    let _ = name.len();
-}
-
-#[test]
-fn tailscale_ip_returns_string() {
-    let ip = sys::tailscale_ip();
-    let _ = ip.len();
-}
-
-#[test]
 fn power_status_returns_tuple() {
     let (on_bat, cap) = sys::power_status();
     // Shouldn't panic regardless of platform
